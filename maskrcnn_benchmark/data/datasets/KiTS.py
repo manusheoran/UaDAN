eb8ceaa7c7d600e0728f975840340fcadb4ae051
Manu <42864020+manusheoran@users.noreply.github.com>
2022-04-27 22:22:09 +0530

    Update KiTS.py

--- maskrcnn_benchmark/data/datasets/KiTS.py
+++ maskrcnn_benchmark/data/datasets/KiTS.py
@@ -17,11 +17,12 @@ from maskrcnn_benchmark.data.transforms.build import build_d2transforms
 class KiTSDataset(object):
 
     def __init__(
-        self, split, data_dir, ann_file, transforms=None
+        self, split, data_dir, ann_file, transforms=None, is_source= True
     ):
         self.transforms = transforms
         self.split = split
         self.data_path = data_dir
+        self.is_source = is_source
         self.classes = ['__background__',  # always index 0
                         'lesion']
         self.num_classes = len(self.classes)
@@ -105,6 +106,8 @@ class KiTSDataset(object):
         classes = torch.ones(num_boxes, dtype=torch.int)  # lesion/nonlesion
         target.add_field("labels", classes)
         
+        domain_labels = torch.ones_like(classes, dtype=torch.uint8) if self.is_source else torch.zeros_like(classes, dtype=torch.uint8)
+        target.add_field("is_source", domain_labels)
 
   
 
