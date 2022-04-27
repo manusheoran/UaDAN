c5824dd6eba76a69d9ba1d39fd4aaba8cb1a1a54
Manu <42864020+manusheoran@users.noreply.github.com>
2022-04-27 20:22:49 +0530

    Update __init__.py

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -3,7 +3,22 @@ from .coco import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
 from .DeepLesion import DeepLesionDataset
+
+from .target_combined_DL import DL_target_combinedDataset
+
 from .LiTS import LiTSDataset
 
+from .KiTS import KiTSDataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset","DeepLesionDataset","LiTSDataset" ]
+from .Ircadb import ircadbDataset
+
+
+from .target_combined_uda import combinedDataset_uda
+from .target_combined_fewshot import combinedDataset_fewshot
+from .target_combined_pseudo import combinedDataset_pseudo
+
+
+__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset","DeepLesionDataset","LiTSDataset" ,
+          "KiTSDataset", "ircadbDataset","combinedDataset_uda", "combinedDataset_fewshot",
+           "combinedDataset_pseudo", "DL_target_combinedDataset"
+          ]
